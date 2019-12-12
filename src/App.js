e80206fe72d4048ede48385b714b74343ba80ff7
Alvin Ng <ngalvin93@gmail.com>
2019-12-11 23:08:06 -0600

    standard

--- src/App.js
+++ src/App.js
@@ -15,10 +15,7 @@ class App extends React.Component {
   }
 
   componentDidMount () {
-    // console.log('did mount')
-    // this.writeUserData()
     const ref = Firebase.database().ref('/')
-    // console.log(ref)
     ref.on('value', snapshot => {
       console.log('snapshot...', snapshot.val())
     })
--- src/components/Module.js
+++ src/components/Module.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 
 export default class Module extends Component {
-    render() {
-        return (
-            <div>
-                <h1>SINGLE MODULE</h1>
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <h1>SINGLE MODULE</h1>
+      </div>
+    )
+  }
 }
