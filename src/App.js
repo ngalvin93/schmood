aea5ff4fc34182ff2c8db98e53c35355cf355e4a
Alvin Ng <ngalvin93@gmail.com>
2019-12-17 18:56:54 -0600

    add counter

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import Header from './components/Header'
 import Board from './components/Board'
 import HowToUse from './components/HowToUse'
 import SharedBoard from './components/SharedBoard'
+import { incrementUser } from '../src/firebase-service'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 class App extends React.Component {
   constructor () {
     super()
     Firebase.initializeApp(config)
+  }
+
+  componentDidMount() {
+    incrementUser()
   }
 
   render () {
