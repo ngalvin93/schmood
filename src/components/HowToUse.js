9099c5ddc5880c2f581da203c3ba3796375f189c
Alvin Ng <ngalvin93@gmail.com>
2020-01-11 23:08:38 -0600

    done

--- src/components/HowToUse.js
+++ src/components/HowToUse.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react'
-import { Button, Container, Row, Col } from 'reactstrap'
+import { Container, Row } from 'reactstrap'
 import './HowToUse.css'
 
 export default class HowToUse extends Component {
   render () {
     return (
-      // <div id='textBox'>
-      //   <h1>Issa scmooooood.</h1>
-      //   <h3>Give your mood board a name.</h3>
-      //   <h3>Visualize your ideas by placing images, text and links to your board. </h3>
-      //   <h3>Get the link and share your schmood.</h3>
-      //   <a href='/'><Button color='primary'>Get started</Button></a>
-      // </div>
-
       <div className='onboard'>
         <Container className='onboard-container' fluid='true'>
           <Row className='onboard-container-row d-flex'>
