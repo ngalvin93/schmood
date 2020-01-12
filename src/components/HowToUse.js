cb2f845d2ad6d86848ec8def961da7377c073828
Alvin Ng <ngalvin93@gmail.com>
2020-01-11 19:36:51 -0600

    wip

--- src/components/HowToUse.js
+++ src/components/HowToUse.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react'
-import { Button } from 'reactstrap'
+import { Button, Container, Row, Col } from 'reactstrap'
 import './HowToUse.css'
 
 export default class HowToUse extends Component {
   render () {
     return (
-      <div id='textBox'>
-        <h1>Issa scmooooood.</h1>
-        <h3>Give your mood board a name.</h3>
-        <h3>Visualize your ideas by placing images, text and links to your board. </h3>
-        <h3>Get the link and share your schmood.</h3>
-        <a href='/'><Button color='primary'>Get started</Button></a>
-      </div>
+      // <div id='textBox'>
+      //   <h1>Issa scmooooood.</h1>
+      //   <h3>Give your mood board a name.</h3>
+      //   <h3>Visualize your ideas by placing images, text and links to your board. </h3>
+      //   <h3>Get the link and share your schmood.</h3>
+      //   <a href='/'><Button color='primary'>Get started</Button></a>
+      // </div>
+
+      <React.Fragment>
+        <Container className='onboard-container' fluid='true'>
+          <Row className='onboard-container-row d-flex'>
+            <h3 className='onboard-container-text'>Give your mood board a name.</h3>
+          </Row>
+        </Container>
+        <Container className='onboard-container' fluid='true'>
+          <Row className='onboard-container-row d-flex'>
+            <h3 className='onboard-container-text'>Visualize your ideas by placing images, text and links to your board.</h3>
+          </Row>
+        </Container>
+        <Container className='onboard-container' fluid='true'>
+          <Row className='onboard-container-row d-flex'>
+            <h3 className='onboard-container-text'>Get the link and share your schmood.</h3>
+          </Row>
+        </Container>
+        <Container className='onboard-container' fluid='true'>
+          <Row className='onboard-container-row d-flex'>
+            <h3 className='onboard-container-text'>Inspire your friends, colleauges and family.</h3>
+          </Row>
+        </Container>
+      </React.Fragment>
     )
   }
 }
